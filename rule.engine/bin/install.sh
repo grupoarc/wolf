020a59fb1359de43c932a0b4b91fb3ef0e6a5b43
Paul Jimenez <pj@place.org>
2014-10-29 11:31:28 -0400

    Fix rule.engine install script

--- rule.engine/bin/install.sh
+++ rule.engine/bin/install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -e
 
 # remove all previous java
-sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\*
+sudo apt-get -y purge openjdk-\* icedtea-\* icedtea6-\*
 
 # install java 1.7
-sudo apt-get install openjdk-7-jdk
+sudo apt-get -y install openjdk-7-jdk
 
-sudo apt-get install zookeeper
+sudo apt-get -y install zookeeper
 # zookeeper conf is in /etc/zookeeper/conf/zoo.cfg
 
 # we will need storm
-sudo apt-get install unzip
+sudo apt-get -y install unzip
 wget http://mirror.symnds.com/software/Apache/incubator/storm/apache-storm-0.9.2-incubating/apache-storm-0.9.2-incubating.zip
 unzip apache-storm-0.9.2-incubating.zip
 
 # we will also need storm-kafka integration jars in mvn repository
 # this step might be done on dev machine, not production server
 # this step will install storm-core 0.9.3 and storm-kafka 0.9.3 and kafka_2.9.2 0.8.1.1 to local mvn repository
-sudo apt-get install maven2 
+sudo apt-get -y install maven
 git clone https://github.com/apache/incubator-storm.git
-cd incubator-storm && mvn clean install -DskipTests=true && cd
+cd incubator-storm && mvn clean install -DskipTests=true && cd ..
 # once this is done we can install rule engine
 cd wolf/rule.engine && mvn clean install
 
